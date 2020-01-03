5c8340c5eb0db28664fa4735fcc1b2bd1ccc56c8
Hany Hamed <h.hamed.elanwar@gmail.com>
2020-01-03 23:45:44 +0300

    [Testing] Test part in the code

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -10,6 +10,7 @@ import generate from "./pdf_generator";
 
 window.onload = () => {
     getField().then((field) => {
+        console.log(field)
         let encoded_descr = encodeField(field);
         render(field, encoded_descr);
         createQR(field, encoded_descr);

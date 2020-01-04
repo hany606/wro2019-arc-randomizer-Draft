b69c07d50537fc09d5852be30dfc2f56d6a31ecf
Hany Hamed <h.hamed.elanwar@gmail.com>
2020-01-04 03:53:17 +0300

    [Enhancements] Modify the qr rendering and randomization

--- src/frontend/qr_code_renderer.js
+++ src/frontend/qr_code_renderer.js
@@ -1,31 +1,47 @@
 'use strict';
 
 import encodeField from '../util/encode_field';
+import {randomSet} from "../util/random";
 const QRCode = require('qrcode');
 
 
 
 export default function createQR(field, code) {
-    let canvas = document.getElementById("qr-code-top-right");
+    let canvas = [];
+    canvas[0] = document.getElementById("qr-code-top-right");
+    canvas[1] = document.getElementById("qr-code-top-left");
+    canvas[2] = document.getElementById("qr-code-bottom-left");
+    canvas[3] = document.getElementById("qr-code-bottom-right");
 
-    let encoded_text = document.getElementById("encoded-field");
-    encoded_text.appendChild(document.createTextNode(code));
+    let qr_codes_keys = ["encoded-field-top-right", "encoded-field-top-left", "encoded-field-bottom-left", "encoded-field-bottom-right"];
 
-    QRCode.toCanvas(canvas, code,
-        {
-            width: 226.77,
-            hight: 226.77,
-            color: { dark: "#000000", light : "#ffffff" },
-            errorCorrectionLevel: 'H'
-        },
-        function (error) {
-            if (error) console.error(error);
+
+
+    let random_value = randomSet(4,4);
+
+    for(let i = 1; i <= 4; i++){
+        let index = random_value[i] - 1;
+        
+        if(index == 0){
+            let true_encoded_field_text = document.getElementById("true-encoded-field");
+            true_encoded_field_text.appendChild(document.createTextNode("The true QR-code's for the field is in the "+ String(qr_codes_keys[index].slice(14,qr_codes_keys[index].length))));
         }
-    );
 
-    // var ctx = canvas.getContext("2d");
-    // ctx.lineWidth = "3";
-    // ctx.strokeStyle = "black";
-    // ctx.rect(5, 5, 227, 227);
-    // ctx.stroke();
+        let encoded_text = document.getElementById(qr_codes_keys[index]);
+        encoded_text.appendChild(document.createTextNode(code[index]));
+
+        QRCode.toCanvas(canvas[index], code[index],
+            {
+                width: 226.77,
+                hight: 226.77,
+                color: { dark: "#000000", light : "#ffffff" },
+                errorCorrectionLevel: 'H'
+            },
+            function (error) {
+                if (error) console.error(error);
+            }
+        );
+    
+    }
+
 }
--- src/util/random.js
+++ src/util/random.js
@@ -10,3 +10,11 @@ export function nextIntIn(min, max) {
     return min + Math.floor(Math.random() * (max - min));
 }
 
+export function randomSet(size, max) {
+    var arr = [];
+    while(arr.length < size){
+        var r = nextIntIn(0,max)+1;
+        if(arr.indexOf(r) === -1) arr.push(r);
+    }
+    return arr;
+}
